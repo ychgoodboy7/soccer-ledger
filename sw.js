723a9cbc90dfc471fa5262e24895e7c5c521b979
ychgoodboy <ychgoodboy2002@163.com>
2026-06-27 23:08:01 +0800

    明细同一天记录按添加时间倒序排列 + SW缓存版本更新至v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'soccer-v1';
+const CACHE = 'soccer-v2';
 const FILES = [
   './index.html',
   './manifest.json',
@@ -8,7 +8,16 @@ const FILES = [
 ];
 
 self.addEventListener('install', e => {
+  e.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
 });
 
 self.addEventListener('fetch', e => {
